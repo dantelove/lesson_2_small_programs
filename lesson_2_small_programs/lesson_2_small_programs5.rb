83c62fe64599935aa5959ac1d812e47df39b0285
Dante Love <outlineandconquer@gmail.com>
2016-07-19 09:27:56 -0700

    Lesson 2 - Mortgage Calculator

--- lesson_2_small_programs/lesson_2_small_programs5.rb
+++ lesson_2_small_programs/lesson_2_small_programs5.rb
@@ -2,32 +2,74 @@
 
 # mortgage calculator
 
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_calculator.yml')
+
 def valid_integer?(num)
-  num.to_i.to_s = num
+  num.to_i.to_s == num
 end
 
-loop do 
+def valid_float?(num)
+  num.to_f.to_s == num
+end
 
-  puts "Enter Loan Amount:"
-  l = gets.chomp.to_f
+def valid_number?(num)
+  valid_integer?(num) || valid_float?(num)
+end
 
-  puts "Enter Annual Percentage Rate (APR):"
-  apr = gets.chomp.to_f
-  c = (apr / 100) / 12
+puts MESSAGES['welcome']
 
-  puts "Enter the Loan Duration in Years:"
-  loan_duration = gets.chomp.to_f
-  n = loan_duration * 12
+loop do # main loop
 
-  result = l*(c*(1 + c)**n)/((1 + c)**n - 1)
+  l = ''
+  loop do
+    puts MESSAGES['loan_amount']
+    l = gets.chomp
+
+    if valid_number?(l) && l.to_f > 0
+      break
+    else
+      puts MESSAGES['invalid_loan_amount']
+    end
+  end
+
+  apr = ''
+  loop do
+    puts MESSAGES['enter_apr']
+    apr = gets.chomp
+
+    if valid_number?(apr) && apr.to_f > 0
+      break
+    else
+      puts MESSAGES['invalid_apr']
+    end
+  end
+
+  c = (apr.to_f / 100) / 12
+
+  loan_duration = ''
+  loop do
+    puts MESSAGES['loan_duration']
+    loan_duration = gets.chomp
+
+    if valid_number?(loan_duration) && loan_duration.to_f > 0
+      break
+    else
+      puts MESSAGES['invalid_loan_duration']
+    end
+  end
+
+  n = loan_duration.to_f * 12
+
+  result = l.to_f*(c*(1 + c)**n)/((1 + c)**n - 1)
 
   puts "Your monthly payment would be about #{result.round(2)}"
 
-  puts "Would you like to calculate another loan?"
-  puts "Type 'YES' to start over."
+  puts MESSAGES['another_loan']
+  puts MESSAGES['start_over']
   input = gets.chomp
   break unless input.downcase == "yes" 
 
 end
 
-puts "Thank you. Goodbye!"
+puts MESSAGES['thank_you']
