28534aa1f56b2533e38a32d53e113dc3d66c46d5
Dante Love <outlineandconquer@gmail.com>
2016-07-20 08:40:55 -0700

    Refactored Rock, Paper, Scissors.

--- lesson_2_small_programs/lesson_2_small_programs6.rb
+++ lesson_2_small_programs/lesson_2_small_programs6.rb
@@ -7,7 +7,6 @@
 puts "Welcome to Rock-Paper-Scissors Extraordinaire!"
 
 loop do # main loop
-
   puts <<-MSG
 
     Type 'R' for Rock.
@@ -20,58 +19,57 @@ loop do # main loop
 
   player_selection.downcase!
 
-    case player_selection
-      when "r"
-        puts "You've selected Rock!"
-        player_selection = "Rock"
-      when "p"
-        puts "You've selected Paper!"
-        player_selection = "Paper"
-      when "s"
-        puts "You've selected Scissors!"
-        player_selection = "Scissors"
-      when "e"
-        break
-      else
-        puts "Please enter R, P, S, or E"
-        redo
-    end
+  case player_selection
+  when "r"
+    puts "You've selected Rock!"
+    player_selection = "Rock"
+  when "p"
+    puts "You've selected Paper!"
+    player_selection = "Paper"
+  when "s"
+    puts "You've selected Scissors!"
+    player_selection = "Scissors"
+  when "e"
+    break
+  else
+    puts "Please enter R, P, S, or E"
+    redo
+  end
 
   cpu_selection = rand(100)
 
   case cpu_selection
-    when 0..32
-      puts "The computer selects Rock!"
-      cpu_selection = "Rock"
-    when 33..65
-      puts "The computer selects Paper!"
-      cpu_selection = "Scissors"
-    when 66..99
-      puts "The computer selects Scissors!"
-      cpu_selection = "Scissors"
+  when 0..32
+    puts "The computer selects Rock!"
+    cpu_selection = "Rock"
+  when 33..65
+    puts "The computer selects Paper!"
+    cpu_selection = "Scissors"
+  when 66..99
+    puts "The computer selects Scissors!"
+    cpu_selection = "Scissors"
   end
 
-  case 
-    when cpu_selection == "Rock" && player_selection == "Rock"
-      puts "TIE! You both choose Rock! Try Again."
-    when cpu_selection == "Paper" && player_selection == "Rock"
-      puts "You Lose! Paper beats rock!"
-    when cpu_selection == "Scissors" && player_selection == "Rock"
-      puts "You Win! Rock beats paper!"
-    when cpu_selection == "Rock" && player_selection == "Paper"
-      puts "You Win! Paper beats rock!"
-    when cpu_selection == "Paper" && player_selection == "Paper"
-      puts "TIE! You both choose Paper! Try Again."
-    when cpu_selection == "Scissors" && player_selection == "Paper"
-      puts "You Lose! Scissors beats paper!"
-    when cpu_selection == "Rock" && player_selection == "Scissors"
-      puts "You Lose! Rock beats scissors!"
-    when cpu_selection == "Paper" && player_selection == "Scissors"
-      puts "You Win! Scissors beats paper!"
-    when cpu_selection == "Scissors" && player_selection == "Scissors"
-      puts "TIE! You both choose Scissors! Try Again."
+  case [cpu_selection, player_selection]
+  when ["Rock","Rock"]
+    puts "TIE! You both choose Rock! Try Again."
+  when ["Paper","Rock"]
+    puts "You Lose! Paper beats Rock!"
+  when  ["Scissors","Rock"]
+    puts "You Win! Rock beats Scissors!"
+  when ["Rock","Paper"]
+    puts "You Win! Paper beats Rock!"
+  when  ["Paper","Paper"]
+    puts "TIE! You both choose Paper! Try Again."
+  when ["Scissors", "Paper"]
+    puts "You Lose! Scissors beats Paper!"
+  when ["Rock","Scissors"]
+    puts "You Lose! Rock beats Scissors!"
+  when ["Paper", "Scissors"]
+    puts "You Win! Scissors beats Paper!"
+  when ["Scissors","Scissors"]
+    puts "TIE! You both choose Scissors! Try Again."
   end
-
 end
 
 puts "Thanks for playing Rock, Paper, Scissors!"
