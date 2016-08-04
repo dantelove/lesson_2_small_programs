914bb143b523d02a68bf3dd18900cba9a5ad326c
Dante Love <outlineandconquer@gmail.com>
2016-08-04 15:53:16 -0700

    LS Solution to 'Medium 1' Exercise

--- lesson_3_exercises/lesson_3_exercises_medium1.rb
+++ lesson_3_exercises/lesson_3_exercises_medium1.rb
@@ -1,8 +1,12 @@
 # lesson_3_exercises_medium1.rb
 
-tab = 0
+#tab = 0
 
-10.times do 
-  puts "The Flintstones Rock!".rjust(tab)
-  tab += 20
-end
+#10.times do 
+#  puts "The Flintstones Rock!".rjust(tab)
+#  tab += 20
+#end
+
+#LS Solution
+
+10.times {|number| puts (' ' * number) + "The Flinstones Rock!"}
