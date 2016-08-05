817116b9e7532b0a98dfff666e3fb7568c822f31
Dante Love <outlineandconquer@gmail.com>
2016-08-05 14:50:13 -0700

    Lesson 3 'Medium 3' Exercises

---
+++ lesson_3_exercises/lesson_3_exercises_medium3_3.rb
@@ -0,0 +1,5 @@
+# lesson_3_exercises_medium3_3.rb
+
+#will display:
+# My string looks like this now: pumpkins
+# My array looks like this now: ["pumpkins", "rutabaga"]
---
+++ lesson_3_exercises/lesson_3_exercises_medium3_5.rb
@@ -0,0 +1,12 @@
+# lesson_3_exercises_medium3_5.rb
+
+def color_valid(color)
+  true if color == "blue" || color == "green"
+end
+
+#LS solution
+
+def color_valid(color)
+  color == "blue" || color "green"
+end
+
